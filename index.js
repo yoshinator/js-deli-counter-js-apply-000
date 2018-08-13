188d39d1831969fdf0493728fde7fd865abb21c1
yoan ante <yoanante@gmail.com>
2018-08-13 23:20:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 /**
   1. Build a function that a new customer will use when entering the deli. The function, `takeANumber`, should accept two paramaters: the current line of people, along with the new person's name. The function should return a welcome message including the new person's position in line, such as `"Welcome, Ada. You are number 1 in line."`. And don't go being too programmer-y and give them their index. These are normal people. If they are 7th in line, tell them that. Don't get their hopes up by telling them they are number 6 in line.
 */
+var ticketNumber = 0;
 
-function takeANumber(currentLine, name){
-  currentLine.push(name)
-  return `Welcome, ${name}. You are number ${currentLine.length} in line.`
+function takeANumber(currentLine){
+  ticketNumber +=1;
+  currentLine.push(ticketNumber)
+  return `You are number ${ticketNumber}.`
   
 }
+let array = []
 
+console.log(takeANumber(array, ticketNumber))
+console.log(array)
+console.log(takeANumber(array, ticketNumber))
+console.log(array)
+console.log(takeANumber(array, ticketNumber))
+console.log(array)
 
 /**
   2. Build a function `nowServing`. This function should accept the current line of people (`katzDeliLine`) and return the first person in line and then remove that individual from the line. If there is nobody in line, it should return "There is nobody waiting to be served!
 */
 
 function nowServing(currentLine){
-  if (currentLine.length === 0)
-    return "There is nobody waiting to be served!"
-  else
+  
+  if (currentLine.length !== 0){
     var rvalue = currentLine[0] //need a temporary place holder since we will be deleting the first person from the array with the shift method
     currentLine.shift();
     return `Currently serving ${rvalue}.`;
+  } else {
+    return "There is nobody waiting to be served!"
+  }
 }
 
 
@@ -28,12 +39,14 @@ function nowServing(currentLine){
 */
 function currentLine(line){
   var i;
+  var temp = []
   if (line.length){
     for (i=0; i < line.length; i++){
-      line[i]= ` ${i+1}. ${line[i]}` //we are replacing the original element with a new element. We prepend a space and an ordered number followed by a period and space then add the original element so that the array elements are formatted in the way we want. 
+      temp[i] = ` ${i+1}. ${line[i]}` //we are replacing the original element with a new element. We prepend a space and an ordered number followed by a period and space then add the original element so that the array elements are formatted in the way we want. 
     }
-    return "The line is currently:" + line;
+    return "The line is currently:" + temp;
   }else{
     return "The line is currently empty."
   }
 }
+
